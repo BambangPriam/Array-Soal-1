0fe4918e5e423660c46bc87f054195c4e7413e9f
BambangPriam <45525619+BambangPriam@users.noreply.github.com>
2019-01-05 11:50:37 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+
+void main(){
+int nilai[5]={5,10,6,0,4}, i;
+
+printf("Array A : \n");
+for(i=0;i<5;i++){
+    printf("Elemen ke-%i : %d \n", i, nilai[i]);
+}
+printf("\n");
+
+printf("Array B : \n");
+
+for(i=0;i<5;i++){
+    printf("Elemen ke-%i : %d \n", i, nilai[i]);
+}
+}
